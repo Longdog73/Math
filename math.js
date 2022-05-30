c772cd121ef02e95a70f542dbe9c89a9662804fd
Longdog73 <cafinn@optusnet.com.au>
2022-05-30 19:34:13 +1000

    a

--- math.js
+++ math.js
@@ -2,13 +2,13 @@
 var i = 0;
 
 $(document).click(function() {
-  var a = Math.floor(Math.random()*8 + 3); // 3 to 10
-  if (a == 10) {
-    a = 11;
+  var a = Math.floor(Math.random()*9 + 3); // 3 to 12
+  if (a >= 10) {
+    a++;
   }
   var b = Math.floor(Math.random()*8 + 3);
-  if (b == 10) {
-    b = 11;
+  if (b >= 10) {
+    b++;
   }
   $(".math").text(a + " x " + b);
   $(".answer").text(a*b);
